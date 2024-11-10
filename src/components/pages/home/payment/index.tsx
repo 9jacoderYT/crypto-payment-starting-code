f3da783b4061539ec27956608d11b7408d3e6f6c
Smart Oghenemine <smartjeremy94@gmail.com>
2024-11-10 20:50:22 +0100

    first commit

--- src/components/pages/home/payment/index.tsx
+++ src/components/pages/home/payment/index.tsx
@@ -21,93 +21,13 @@ export default function PaymentSection() {
   const [error, setError] = useState<string | null>("");
   const [success, setSuccess] = useState<boolean | null>(false);
 
-  const fetchTokenAmount = async (usdAmount: number, token: string) => {
-    try {
-      const response = await fetch("/api/convert-price", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ usdAmount, token }),
-      });
+  const fetchTokenAmount = async (usdAmount: number, token: string) => {};
 
-      if (!response.ok) {
-        throw new Error(`Error: ${response.statusText}`);
-      }
-
-      const data = await response.json();
-      return data.amount; // Return the fetched amount
-    } catch (err) {
-      // Return fallback values in case of an error
-      return 0;
-    }
-  };
-
-  const handlePayment = async () => {
-    setError(null);
-    setSuccess(null);
-
-    if (!currency || !wallet || !amount || !transactionHash) {
-      setError("All fields are required");
-      return;
-    }
-
-    setLoading(true);
-
-    try {
-      const url = `/api/validate-${currency}`;
-
-      const response = await fetch(url, {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          transactionHash: transactionHash,
-          wallet: wallet,
-          amount: amount,
-          currency: currency,
-        }),
-      });
-
-      const { error, message } = await response.json();
-
-      if (error) {
-        setError(message);
-        return;
-      }
-
-      setSuccess(true);
-    } catch (error) {
-      console.log(error);
-    } finally {
-      setLoading(false);
-    }
-  };
+  const handlePayment = async () => {};
 
   const handleCurrencyChange = async (
     e: React.ChangeEvent<HTMLSelectElement>
-  ) => {
-    setError(null);
-    const currencyValue = e.target.value;
-    if (currencyValue == "null" || !amount) {
-      setWallet("");
-      setAmount(0);
-      return;
-    }
-
-    if (currencyValue == "usdt") {
-      setWallet("0x0827BC11F147ABdB20aDF6b5Ff8204A7eEFA165F");
-    }
-    if (currencyValue == "bnb") {
-      setWallet("0x0827BC11F147ABdB20aDF6b5Ff8204A7eEFA165F");
-      // convert the currency
-      const currency = await fetchTokenAmount(amount, "bnb");
-      setAmount(currency);
-    }
-
-    setCurrency(currencyValue);
-  };
+  ) => {};
 
   const copyToClipboard = async () => {
     try {
@@ -156,46 +76,6 @@ export default function PaymentSection() {
               <option value="bnb">BNB - BSC Network</option>
             </select>
 
-            {currency && (
-              <>
-                <div className="text-white text-xl my-5 w-full">
-                  Send Exactly{" "}
-                  <big className="text-teal-400">
-                    {amount} {currency}
-                  </big>{" "}
-                  here
-                  <b />
-                  <p className="text-sm break-words overflow-hidden">
-                    {wallet}
-                    <button
-                      onClick={copyToClipboard}
-                      className="cursor-pointer ml-2"
-                    >
-                      <CopyAllOutlined className="w-5 h-5 text-gray-500 hover:text-teal-700 transition duration-200" />
-                    </button>
-                  </p>
-                </div>
-
-                <input
-                  type="text"
-                  placeholder="Transaction Hash"
-                  className="w-full h-12 px-4 text-teal-700 transition duration-200 bg-white border border-transparent rounded shadow-md focus:border-teal-900 focus:outline-none"
-                  onChange={(event) =>
-                    setTransactionHash(event.target.value.trim())
-                  }
-                />
-
-                <div className="my-3 font-light text-white italic">
-                  Your Valid Transaction Hash
-                </div>
-
-                <p className="max-w-md px-5 mb-3 text-xs text-white sm:text-sm md:mb-5">
-                  I confirm I have made exactly {amount} {currency} transfer to
-                  the wallet address.
-                </p>
-              </>
-            )}
-
             <div className="flex items-center w-full mb-5">
               <hr className="flex-1 border-gray-300" />
               <div className="px-3 text-xs text-gray-500 sm:text-sm">
@@ -214,18 +94,6 @@ export default function PaymentSection() {
                 <CurrencyExchange className="ml-2" />
               </button>
             </div>
-
-            {error && (
-              <>
-                <Alert severity="error">{error}</Alert>
-              </>
-            )}
-
-            {success && (
-              <>
-                <Alert severity="success">Transaction is Valid.</Alert>
-              </>
-            )}
           </div>
         </div>
       </div>
